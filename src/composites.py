7cdb3195aeb928da29a4e4c10a9e5ea6f4d869ea
Vangelis Fotakidis <vfotakidis96@gmail.com>
2025-08-15 03:51:21 +0300

    backbone of pipeline ready

--- src/composites.py
+++ src/composites.py
@@ -1,4 +1,6 @@
 import datacube
+from datacube.index.hl import Doc2Dataset
+from eodatasets3 import serialise
 
 import pystac_client
 import pystac
@@ -13,6 +15,7 @@ import odc.geo.xr
 import pandas as pd
 import numpy as np
 import rioxarray as rxr
+from rioxarray.merge import merge_datasets
 import xarray as xr
 from odc.algo import mask_cleanup
 from dea_tools.spatial import xr_rasterize
@@ -42,6 +45,9 @@ from typing import List, Tuple
 
 import warnings
 warnings.filterwarnings("ignore")
+
+from utils.downsample import s2_downsample_dataset_10m_to_20m
+from utils.metadata import prepare_eo3_metadata_NAS
 
 
 def mkdir(path: str):
@@ -134,7 +140,8 @@ def check_gri_refinement(items: List[pystac.Item]) -> Tuple[List[pystac.Item], p
     refinement_data = []
 
     # Loop through items and extract `Image_Refining` flag
-    for item in items:
+    
+    for i, item in enumerate(items):
         datastrip_metadata_url = None
         for asset_key, asset_data in items[0].assets.items():
             if "datastrip-metadata" in asset_key.lower(): # and asset_data.href.endswith(".xml"):
@@ -155,7 +162,7 @@ def check_gri_refinement(items: List[pystac.Item]) -> Tuple[List[pystac.Item], p
                 refining_element = root.find(".//Geometric_Info/Image_Refining")
                 refining_flag = refining_element.get("flag") if refining_element is not None else "Not Found"
 
-                logging.info(f"{item.id} -> {refining_flag}")
+                logging.info(f"{i}/{len(items)} - {item.id} -> {refining_flag}")
 
                 # Store item and status in dataframe
                 refinement_data.append({
@@ -185,6 +192,27 @@ def check_gri_refinement(items: List[pystac.Item]) -> Tuple[List[pystac.Item], p
     return refined_items, df_refinement_status
 
 
+def mask_with_scl(ds, bands):
+    # - 0: no data
+    # - 1: saturated or defective
+    # - 2: dark area pixels
+    # - 3: cloud shadows
+    # - 4: vegetation
+    # - 5: bare soils
+    # - 6: water
+    # - 7: unclassified
+    # - 8: cloud medium probability
+    # - 9: cloud high probability
+    # - 10: thin cirrus
+    # - 11: snow or ice
+    invalid_scl_values = [3, 7, 8, 9, 10, 11]
+    logging.info(f'Masking bits: {invalid_scl_values}')
+    cloud_binary_mask = ds.SCL.isin(invalid_scl_values)
+
+    bands.remove('SCL')    
+    ds = ds[bands].where(~cloud_binary_mask, 0).astype('uint16')
+    
+    return ds
 
 
 def generate_composite(year_month: str, tile: pd.Series):
@@ -323,106 +351,247 @@ def generate_composite(year_month: str, tile: pd.Series):
 
     log.info(f'                                 ')
     log.info(f'Downstream STAC items from Planetary Computer')
-    BANDS_R10m = ['B02', 'B03', 'B04', 'B08']
-    BANDS_R20m = ['B05', 'B06', 'B07', 'B8A', 'B11', 'B12', 'SCL']
-    
+    BANDS_R10m = ['B02', 'B03', 'B04']
+    BANDS_R20m = ['B05', 'B07', 'B8A', 'SCL']
+
 
     # Loading for each EPSG and Native RESOLUTION separatelly, then merging into a single xr.Dataset
     # to to a correct downsampling
-    for EPSG in epsgs:
-        for RESOLUTION in [10, 20]:
-            if RESOLUTION==10:
-                BANDS=BANDS_R10m
-            else:
-                BANDS=BANDS_R20m
-            
-            log.info('    Loading parameters:')
-            log.info(f'        Bands: {BANDS}')
-            log.info(f'        Spatial resolution: {RESOLUTION}')
+    try:
+        processed_epsgs = []
+        for EPSG in epsgs:
+            processed_bands = []
+            geobox = None
+            log.info('Loading bands of diferent resolutions')
+            for RESOLUTION in [20, 10]:
+                if RESOLUTION==10:
+                    BANDS=BANDS_R10m
+                else:
+                    BANDS=BANDS_R20m
+                
+                log.info('                       ')
+                log.info('    Loading parameters:')
+                log.info(f'        Bands: {BANDS}')
+                log.info(f'        Spatial resolution: {RESOLUTION}')
 
+                
+                ds_cube = odc.stac.stac_load(
+                    filtered_items,
+                    bbox=aoi_bbox,
+                    bands=BANDS,
+                    chunks=dict(y=2048, x=2048),
+                    crs=f'EPSG:{EPSG}',  # {epsgs[0]}
+                    resolution=RESOLUTION,
+                    groupby='time', # if 'time' loads all items, retaining duplicates
+                    fail_on_error=True,
+                    # resampling={
+                    #     "*": RESAMPLING_ALGO,
+                    # },
+                ).compute()
+                
+                if RESOLUTION==10:
+                    log.info('        Downsample 10m bands to 20m by average 2x2 binning')
+                    ds_cube = s2_downsample_dataset_10m_to_20m(ds_cube)
+                    log.info(f'        Warp (reproject) 2x2 binned bands like native 20m bands: method={RESAMPLING_ALGO}')
+                    ds_bands = ds_bands.odc.reproject(how=geobox, resampling=RESAMPLING_ALGO)
+                elif RESOLUTION==20:
+                    log.info('        Fix order of dimensions')
+                    ds_bands = ds_bands[['time','y','x']+list(ds_bands.data_vars)]
+                    geobox = ds_bands.odc.geobox
+                
+                log.info(f'        Append bands to the band-list of EPSG:{EPSG}')
+                processed_bands.append(ds_bands)
             
-            ds_cube = odc.stac.stac_load(
-                filtered_items,
-                bbox=aoi_bbox,
-                bands=BANDS,
-                chunks=dict(y=2048, x=2048),
-                crs=f'EPSG:{EPSG}',  # {epsgs[0]}
-                resolution=RESOLUTION,
-                groupby='time', # if 'time' loads all items, retaining duplicates
-                fail_on_error=True,
-                # resampling={
-                #     "*": RESAMPLING_ALGO,
-                # },
+            log.info('Merging bands in a single dataset')
+            log.info('    Ensure all partial datasets are at 20m resolution')
+            for dataset in processed_bands:
+                dataset.odc.resolution == 20
+            
+            log.info('    Clip to the intersection of indexes')
+            xmin = xmax = ymin = ymax = None
+            for da in processed_bands:
+                xmin = da.x.min().item() if xmin is None or da.x.min() > xmin else xmin
+                xmax = da.x.max().item() if xmax is None or da.x.max() < xmax else xmax
+                ymin = da.y.min().item() if ymin is None or da.y.min() > ymin else ymin
+                ymax = da.y.max().item() if ymax is None or da.y.max() < ymax else ymax
+
+            merge_bands = []
+            log.info('    Make a list of bands to merge')
+            for da in processed_bands:
+                merge_bands.append(da.sel(x=slice(xmin, xmax), y=slice(ymax,ymin)))
+            
+            log.info('    Merge on intersection')
+            ds_epsg = xr.merge(
+                merge_bands,
+                compat="no_conflicts",
+                combine_attrs="drop_conflicts",
+                join="inner"
             )
             
-            if RESOLUTION==10:
+            log.info(f'    Apply masks on clouds, shadows, thin cirrus, and snow/ice')
+            BANDS = ['B02', 'B03', 'B04', 'B05', 'B07', 'B08', 'B8A', 'SCL']
+            ds_epsg_masked = mask_with_scl(ds_epsg, BANDS)
+            
+            RESAMPLING_ALGO = "bilinear"
+            EPSG = '3035'
+            log.info(f'    Reproject to Tiling Schema projection EPSG:{EPSG}')
+            ds_epsg_masked = ds_epsg_masked.odc.reproject(how=f'EPSG:{EPSG}', resampling=RESAMPLING_ALGO)
+            
+            processed_epsgs.append(ds_epsg_masked)
+               
+        
+        if len(processed_epsgs)>1:
+            log.info(f'                          ')
+            log.info(f'Mosaic datasets of different native UTM zones to a single dataset')
+            ds_timeseries = merge_datasets(processed_epsgs)
+        else:
+            ds_timeseries = processed_epsgs[0]
+        # reset bands list
+        BANDS = ['B02', 'B03', 'B04', 'B05', 'B07', 'B08', 'B8A']
+        
+        print('Clip value range')
+        chunks = {"time": 1, "y": 1024, "x": 1024}
+        ds_timeseries = ds_timeseries.chunk(chunks)
+        ds_timeseries = ds_timeseries.where(ds_timeseries > 0, np.nan)
+        ds_timeseries = ds_timeseries.where(ds_timeseries<=10000)
+        
+                        
+        log.info(f'                          ')
+        log.info('Computing spectral indices:')
+        SIS = ['evi', 'ndvi', 'psri2']
+
+        log.info('    EVI...')
+        ds_timeseries['evi'] = 2.5 * ((ds_timeseries.B8A - ds_timeseries.B04)/10000) / ((ds_timeseries.B8A/10000 + 6*ds_timeseries.B04/10000 - 7.5*ds_timeseries.B02/10000) + 1)
+        log.info('    NDVI...')
+        ds_timeseries['ndvi'] = ((ds_timeseries.B05 - ds_timeseries.B03) / ds_timeseries.B07).astype('float32')
+        log.info('    PSRI2...')
+        ds_timeseries['psri2'] = ((ds_timeseries.B8A - ds_timeseries.B04) / (ds_timeseries.B8A + ds_timeseries.B04)).astype('float32')
+
+        log.info('Clip to typical value range')
+        for si in SIS:
+            if si in ['ndvi', 'evi']:
+                ds_timeseries[si] = ds_timeseries[si].where((ds_timeseries[si]>=-1)&(ds_timeseries[si]<=1))
+            else:
+                ds_timeseries[si] = ds_timeseries[si].where((ds_timeseries[si]>=-1)&(ds_timeseries[si]<=4))
+        
+        log.info('Reducing to median value temporal composite')
+        # ds_timeseries = ds_timeseries.chunk(chunks)
+        ds_timeseries = ds_timeseries.sortby('time')
+        composite = ds_timeseries.median(dim='time').astype('float32').compute()
+        
+        log.info('Define data types and nodata per band')
+        VARS = BANDS+SIS
+
+        for band in BANDS:
+            dtype = 'uint16'
+            nodata = np.iinfo(np.uint16).min #0
+            composite[band] = composite[band].astype(dtype)
+            composite[band] = composite[band].rio.write_nodata(nodata, inplace=True)
+            composite[band].encoding.update({"dtype": dtype})
+            
+        for si in SIS:  
+            scale = 1000 
+            dtype = 'int16'
+            nodata = np.iinfo(np.int16).min #-32768
+            composite[si] = (composite[si]*scale).round()
+            composite[si] = composite[si].fillna(nodata).astype(dtype)
+            composite[si] = composite[si].rio.write_nodata(nodata, inplace=True)
+            composite[si].encoding.update({"dtype": dtype})
+            # composite[si].encoding["scale_factor"] = 1/scale
+
+
+        log.info('Assign time range and tile ID in metadata')
+        yyyy = ds_timeseries.isel(time=0).time.dt.year.item()
+        mm1 = ds_timeseries.isel(time=0).time.dt.month.item()
+        mm2 = ds_timeseries.isel(time=-1).time.dt.month.item()
+        dd1 = ds_timeseries.isel(time=0).time.dt.day.item()
+        dd2 = ds_timeseries.isel(time=-1).time.dt.day.item()
+        composite.attrs['dtr:start_datetime']=f'{yyyy}-{mm1:02d}-{dd1:02d}'
+        composite.attrs['dtr:end_datetime']=f'{yyyy}-{mm2:02d}-{dd2:02d}'
+        composite.attrs['odc:region_code']=tile
+        
+
+        log.info('Create directories and naming conversions')        
+        NASROOT='//nas-rs.topo.auth.gr/Latomeia/DROUGHT'
+        FOLDER=f'COMPOS/{yyyy}/{mm1}/{tile}'
+        DATASET= f'S2L2A_medcomp_{tile}_{yyyy}{mm1:02d}'
+        PRODUCT_NAME = 'composites'
+        collection_path = f"{NASROOT}/{FOLDER}"
+        mkdir(collection_path)
+        eo3_path = f'{collection_path}/{DATASET}.odc-metadata.yaml'
+        stac_path = f'{collection_path}/{DATASET}.stac-metadata.json'
+        datetime_list = [
+            ds_timeseries.isel(time=0).time.dt.year.item(),
+            ds_timeseries.isel(time=0).time.dt.month.item(),
+            1
+        ]
+        
+        
+        log.info('Write bands to raster COG files')
+        name_measurements = []
+        for var in list(composite.data_vars):
+            print(var)
                 
-
+            file_path = f'{collection_path}/{DATASET}_{var}.tif'
             
-
-            log.info(f'        Resampling algorithm: {RESAMPLING_ALGO}')                
+            composite[var].rio.to_raster(
+                raster_path=file_path, 
+                driver='COG',
+                dtype=str(composite[var].dtype),
+                windowed=True
+                )
+            name_measurements.append(file_path)
             
-    
-    
-    
-    BANDS = ['B02', 'B03', 'B04', 'B05', 'B07', 'B08', 'B8A', 'SCL']
-    EPSG = 'EPSG:3035'
-    RESOLUTION = 20
-    RESAMPLING_ALGO = "average"
-    log.info('    Loading parameters:')
-    log.info(f'        Bands: {BANDS}')
-    log.info(f'        Spatial resolution: {RESOLUTION}')
-    log.info(f'        Resampling algorithm: {RESAMPLING_ALGO}')
-    
-    ds_cube = odc.stac.stac_load(
-        filtered_items,
-        bbox=aoi_bbox,
-        bands=BANDS,
-        chunks=dict(y=2048, x=2048),
-        crs=EPSG,  # {epsgs[0]}
-        resolution=RESOLUTION,
-        groupby='time', # if 'time' loads all items, retaining duplicates
-        fail_on_error=True,
-        resampling={
-            "*": RESAMPLING_ALGO,
-        },
-    )
-    
-    log.info(f'Apply masks on clouds, shadows, thin cirrus, and snow/ice')
-    # - 0: no data
-    # - 1: saturated or defective
-    # - 2: dark area pixels
-    # - 3: cloud shadows
-    # - 4: vegetation
-    # - 5: bare soils
-    # - 6: water
-    # - 7: unclassified
-    # - 8: cloud medium probability
-    # - 9: cloud high probability
-    # - 10: thin cirrus
-    # - 11: snow or ice
-    invalid_scl_values = [3, 7, 8, 9, 10, 11]
-    log.info(f'Masking bits: {invalid_scl_values}')
-    cloud_binary_mask = ds_cube.SCL.isin(invalid_scl_values)
+        log.info('Prepare metadata YAML document')
+        eo3_doc, stac_doc = prepare_eo3_metadata_NAS(
+            
+        )
+        
+        log.info('Write metadata YAML document to disk')
+        serialise.to_path(Path(eo3_path), eo3_doc)
+        with open(stac_path, 'w') as json_file:
+            json.dump(stac_doc, json_file, indent=4, default=False)
+        
+        log.info('Create datacube.model.Dataset from eo3 metadata')
+        WORKING_ON_CLOUD=False
+        uri = eo3_path if WORKING_ON_CLOUD else f"file:///{eo3_path}"
 
-    BANDS.remove('SCL')    
-    ds_cube_cf = ds_cube[BANDS].where(~cloud_binary_mask, 0).astype('uint16')
+        resolver = Doc2Dataset(dc.index)
+        dataset_tobe_indexed, err  = resolver(doc_in=serialise.to_doc(eo3_doc), uri=uri)
+        
+        if err:
+            log.error(err)
+            
+        log.info('Index to datacube')
+        dc.index.datasets.add(dataset=dataset_tobe_indexed, with_lineage=False)
+        
+    except Exception as exc:
+        msg=f'Failed loading for : Tile {tile_id} | Time: {year_month}\nwith Exception: {exc}'
+        log.error(msg)
+        client.close()
+        cluster.close()
+        return
     
-    ds_cube_cf = ds_cube_cf.where(ds_cube_cf > 0, np.nan)
+    
+    
+    
+    
+
+    
     try:
         log.info(f'Downloading bands and computing median composites: Tile {tile_id} | Time: {year_month}')
-        median_composite = ds_cube_cf[BANDS].median(dim='time').astype('float32')
-        median_composite = median_composite.compute()
+        composite = ds_cube_cf[BANDS].median(dim='time').astype('float32')
+        composite = composite.compute()
         
         log.info('Convert to unsigned integer 16-bits')
-        median_composite = median_composite.where(~median_composite.isnull(), 0).astype('uint16')
+        composite = composite.where(~composite.isnull(), 0).astype('uint16')
 
-        for var in list(median_composite.data_vars):
-            median_composite[var].attrs['nodata'] = 0
+        for var in list(composite.data_vars):
+            composite[var].attrs['nodata'] = 0
 
         # Convert to a multi-band DataArray
-        da = xr.concat([median_composite[var] for var in median_composite.data_vars], dim="band")
-        da = da.assign_coords(band=("band", list(range(1, len(median_composite.data_vars) + 1))))
+        da = xr.concat([composite[var] for var in composite.data_vars], dim="band")
+        da = da.assign_coords(band=("band", list(range(1, len(composite.data_vars) + 1))))
         da.name = ''
         
         FOLDER_NAME = f"../composites/AOI{AOI_number}"
